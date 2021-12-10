a519174566f9b7590c1a360c9346de630d6d1278
raajeev.ranjann <raajeev.ranjann@gmail.com>
2021-12-10 12:31:01 +0530

    close popup on outside click done

--- src/components/UI/popup/Popup.js
+++ src/components/UI/popup/Popup.js
@@ -3,6 +3,10 @@ import style from "./Popup.module.css";
 
 const Popup = (props) => {
   const [trigger, setTrigger] = useState(props.trigger);
+  document.addEventListener("click", () => {
+    trigger && setTrigger(!trigger);
+  });
+
   return trigger ? (
     <div className={style.popup}>
       <div className={style.popup_inner}>
